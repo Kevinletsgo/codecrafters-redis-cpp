940af52dfbc1b2e18e9069084e01f679c1a2bf24
Kevinletsgo <jinkaiwen666@gmail.com>
2024-11-27 19:38:44 +0000

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,9 +10,40 @@
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <sstream>
+
+using std::cout;
 std::mutex logMutex; // 控制日志输出的互斥锁
 std::vector<std::thread> threads; // 存储所有线程
+
+void parserRedis(std::string& input, std::vector<std::string> &result) {
+  std::istringstream stream(input);
+  // std::cout << "input:" << input << '\n';
+  
+  std::string line;
+  std::getline(stream, line, '\r');
+  stream.get();
+  std::cout << "line:" << line << '\n';
+    if (line.empty() || line[0] != '*') {
+      throw std::invalid_argument("Invalid RESP input: Expected '*'");
+  }
+  int numEle = std::stoi(line.substr(1));//"*2\r\n$2\r\nhi\r\n$4\r\nbaby\r\n"
+  for (int i = 0; i < numEle; ++i) {
+    // Read the bulk string header 
+    std::getline(stream, line, '\r');
+    stream.get();
+    if (line.empty() || line[0] != '$') {
+      throw std::invalid_argument("Invalid RESP input: Expected '$'");
+    }
+    int strLength = std::stoi(line.substr(1)); // Extract string length
+    // Read the string content
+    std::getline(stream, line, '\r');
+    stream.get();
+    result.emplace_back(line); // Add the string to result
+  }
+}
 void handleClient(int client_fd) {
+    std::vector<std::string> result;
     char buffer[1024];
     while (true) {
         memset(buffer, 0, sizeof(buffer));
@@ -24,10 +55,26 @@ void handleClient(int client_fd) {
         }
         std::lock_guard<std::mutex> lock(logMutex);
         std::cout << "Received: " << buffer << std::endl;
-        std::string response = "+PONG\r\n";
-        send(client_fd, response.c_str(), response.size(), 0);//     
+        std::string input = buffer;
+        parserRedis(input, result);
+        //reply Echo
+        if(strcasecmp(result[0].c_str(), "Echo") == 0) {
+          cout << "Echo success!"<< '\n';
+          std::ostringstream oss;
+          for(int i = 1; i < result.size(); ++i) {
+            auto buf = result[i];
+            oss << "$"  << buf.size() << "\r\n" << buf <<"\r\n";
+          }
+          std::string res = oss.str();
+          int ret = send(client_fd, res.c_str(), res.size(), 0); //不能用sizeof(res.c_str())因为这里计算的是指针的大小
+          if(ret == -1) {
+            std::cerr << "Send failed with errno: " << errno << std::endl;
+          }
+        }
+        //std::string response = "+PONG\r\n";
+        //send(client_fd, response.c_str(), response.size(), 0);//发送PONG给客户端 
     }
-    close(client_fd); // 关闭客户端连接
+    close(client_fd); // 
 }
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -45,7 +92,7 @@ int main(int argc, char **argv) {
    return 1;
   }
   //
-  // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // // Since the tester restarts your program quite often, settin./g SO_REUSEADDR
   // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
@@ -88,3 +135,5 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+
